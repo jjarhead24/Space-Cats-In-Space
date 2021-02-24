a1dbc61dc18970045f04ec2d6880c0ca5bcc50fd
Jared Seymour <jarexjared@gmail.com>
2021-02-24 15:21:09 +0000

    added death and success sounds
    more sounds more pounds?

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -1,25 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour
 {
     [SerializeField] float RCSThrust = 100f;
     [SerializeField] float ThrusterPower = 100f;
+    [SerializeField] AudioClip Engine;
+    [SerializeField] AudioClip LvlComplete;
+    [SerializeField] AudioClip Shocked;
     Rigidbody rigid; //this is a variable
-    AudioSource Boosters;
+    AudioSource SoundPlayer;
+
+    enum State { Alive, Dying, Trancending }
+    State CurrentState = State.Alive;
 
     // Start is called before the first frame update
     void Start()
     {
         rigid = GetComponent<Rigidbody>();
-        Boosters = GetComponent<AudioSource>();
+        SoundPlayer = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        ProcessInput();
+        if (CurrentState == State.Alive)
+        {
+            ProcessInput();
+        }
     }
 
     private void ProcessInput()
@@ -30,11 +40,16 @@ public class Rocket : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
+        if (CurrentState != State.Alive) {return;}
+
         switch (collision.gameObject.tag)
         {
             case "Death":
                 {
-                    print("Dead");
+                    CurrentState = State.Dying;
+                    SoundPlayer.Stop();
+                    SoundPlayer.PlayOneShot(Shocked);
+                    Invoke("LoadAfterDeath", 2.2f);
                     break;
                 }
             case "Energy":
@@ -42,12 +57,31 @@ public class Rocket : MonoBehaviour
                     print("Energy");
                     break;
                 }
+            case "Finish":
+                {
+                    CurrentState =State.Trancending;
+                    SoundPlayer.Stop();
+                    SoundPlayer.PlayOneShot(LvlComplete);
+                    Invoke("LoadNextLvl", 1f); 
+                    break;
+                }
             case "":
                 {
-                    print("OK");
                     break;
                 }
         }
+    }
+
+    private void LoadAfterDeath()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    private void LoadNextLvl()
+    {
+
+        
+        SceneManager.LoadScene(1); //allow more levels to load
     }
 
     private void Rotate()
@@ -75,14 +109,15 @@ public class Rocket : MonoBehaviour
         if (Input.GetKey(KeyCode.Space))
         {
             rigid.AddRelativeForce(Vector3.up * ThrustSpeed);
-            if (!Boosters.isPlaying)
+            if (!SoundPlayer.isPlaying)
             {
-                Boosters.Play();
+                SoundPlayer.PlayOneShot(Engine);
             }
+            
         }
         else
         {
-            Boosters.Stop();
+            SoundPlayer.Stop();
         }
     }
 }
